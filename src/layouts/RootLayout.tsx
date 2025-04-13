9bbb81d1c4672261a963aea08fbe57363f6aa0bd
Alex Valle <alexvalle75@gmail.com>
2025-04-13 22:41:23 +0200

    Changed Root layout

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -4,10 +4,12 @@ import Footer from '../components/Footer';
 
 export default function RootLayout() {
   return (
-    <div className="flex flex-col min-h-dvh">
-      <Header />
-      <Outlet />
+    <>
+      <div className="flex flex-col min-h-dvh">
+        <Header />
+        <Outlet />
+      </div>
       <Footer />
-    </div>
+    </>
   );
 }
