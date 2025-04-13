955e935570c55bad3a2b52ee3c56c6cd29393885
Alex Valle <alexvalle75@gmail.com>
2025-04-13 15:31:31 +0200

    Refactor app-components folder to pages folder

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
-import RootLayout from './app-components/RootLayout.tsx';
-import HomePage from './app-components/HomePage.tsx';
+import RootLayout from './pages/RootLayout.tsx';
+import HomePage from './pages/HomePage.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
