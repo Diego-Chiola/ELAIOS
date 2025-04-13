18bc75fbb160a676b283bf8218e5089f26bbf87b
Alex Valle <alexvalle75@gmail.com>
2025-04-13 20:04:06 +0200

    Refactor moved components from _components to components folder

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom';
-import Header from '../_components/Header';
-import Footer from '../_components/Footer';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 export default function RootLayout() {
   return (
