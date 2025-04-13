dd8a711c84f02e44a75141d6d5e0aecb31393227
Alex Valle <alexvalle75@gmail.com>
2025-04-13 20:09:20 +0200

    Refactor format tailwind.config file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,42 +1,42 @@
-import type { Config } from "tailwindcss";
-import animate from "tailwindcss-animate";
+import type { Config } from 'tailwindcss';
+import animate from 'tailwindcss-animate';
 
 const config: Config = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     fontFamily: {
-      sans: ["var(--font-sans)", "sans-serif"],
-      serif: ["var(--font-serif)", "serif"],
+      sans: ['var(--font-sans)', 'sans-serif'],
+      serif: ['var(--font-serif)', 'serif'],
     },
     extend: {
       screens: {
-        xs: "420px",
+        xs: '420px',
       },
       backgroundImage: {
-        "text-grad":
-          "linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%)",
+        'text-grad':
+          'linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%)',
       },
       colors: {
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
         primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
         },
         secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
         },
-        border: "hsl(var(--border))",
+        border: 'hsl(var(--border))',
       },
       borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
       },
       borderColor: {
-        DEFAULT: "hsl(var(--border))", // 👈 This is critical
-        border: "hsl(var(--border))",
+        DEFAULT: 'hsl(var(--border))', // 👈 This is critical
+        border: 'hsl(var(--border))',
       },
     },
   },
