19503e70da008f809d86f12ce610c18a2e093002
Alex Valle <alexvalle75@gmail.com>
2025-04-13 22:42:02 +0200

    Changes to homepage

--- src/pages/homepage/HomePage.tsx
+++ src/pages/homepage/HomePage.tsx
@@ -1,13 +1,36 @@
-import Hero from './components/Hero';
+import Container from '../../components/Container';
+import Page from '../../components/Page';
+import LeftBg from './static/left-bg.svg';
+import RightBg from './static/right-bg.svg';
 
 export default function HomePage() {
   return (
-    <div>
-      <Hero />
+    <Page className="flex relative md:mt-0 h-screen-real mt-header">
+      {/* Background images */}
+      {/* <div className="absolute inset-0 pointer-events-none z-[-100]"> */}
+      {/*   <img */}
+      {/*     src={LeftBg} */}
+      {/*     alt="left background" */}
+      {/*     className="object-cover absolute top-0 left-0 w-full h-full bg-red-50" */}
+      {/*   /> */}
+      {/*   <img */}
+      {/*     src={RightBg} */}
+      {/*     alt="right background" */}
+      {/*     className="hidden object-cover absolute top-0 right-0 h-full lg:block" */}
+      {/*   /> */}
+      {/* </div> */}
 
-      <p className="px-8 md:px-0">
-        Exploring Literary and Archaeological Insights on ancient Oil Studies
-      </p>
-    </div>
+      <Container className="mt-12">
+        <main className="flex flex-col justify-center items-center">
+          <h1 className="text-7xl sm:text-9xl xs:text-8xl font-accent md:text-[200px]">
+            ELAIOS
+          </h1>
+        </main>
+      </Container>
+
+      {/* <p className="px-8 font-sans text-7xl md:px-0"> */}
+      {/*   Exploring Literary and Archaeological Insights on ancient Oil Studies */}
+      {/* </p> */}
+    </Page>
   );
 }
