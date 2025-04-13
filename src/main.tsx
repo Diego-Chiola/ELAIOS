44637e80b4a1fe3e3990907a5b83cdcf8ea756f2
Alex Valle <alexvalle75@gmail.com>
2025-04-13 20:08:11 +0200

    Refactor rename index.css to global.css and format

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './index.css';
+import './global.css';
 import RootLayout from './layouts/RootLayout.tsx';
 import HomePage from './pages/homepage/HomePage.tsx';
 
