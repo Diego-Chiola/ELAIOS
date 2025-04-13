b2258a8be9a0d1033202dfe833f569f9e2d27e53
Diego-Chiola <chioladiego@gmail.com>
2025-04-13 18:18:56 +0200

    Modiefied header and added Content.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import Footer from "./_components/footer";
-import Header from "./_components/header";
-import Container from "./components/container";
+import Content from './_components/content';
+import Footer from './_components/footer';
+import Header from './_components/header';
 
 function App() {
   return (
     <main className="flex flex-col min-h-dvh bg-background">
       <Header />
-      <Container className="pt-8 text-2xl font-medium text-center">
-        <p className="px-8 md:px-0">
-          Exploring Literary and Archaeological Insights on ancient Oil Studies
-        </p>
-      </Container>
+      {/* <Content /> */}
       <Footer />
     </main>
   );
--- src/_components/header.tsx
+++ src/_components/header.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { FiMenu, FiX } from "react-icons/fi";
+import { Button } from "../components/ui/button";
 
 const navLinks = [
   { displayedName: "Home", href: "#home" },
@@ -16,14 +17,22 @@ export default function Header() {
   return (
     <header className="bg-primary text-primary-foreground">
       <div className="flex justify-between items-center py-4 md:py-6 px-4 mx-auto w-full md:max-w-9/10">
-        <img className="z-10 w-[175px]" src="./logo.svg" alt="ELAIOS logo" />
+        <div className="text-base font-accent xs:text-lg w-[350px] mr-20">
+          <img className="z-10 w-[175px]" src="./logo.svg" alt="ELAIOS logo" />
+          <h2 className="pl-7.5">
+            Exploring Literary and Archaeological Insights on ancient Oil
+            Studies
+          </h2>
+        </div>
 
         {/* Desktop Nav */}
         <nav className="hidden md:block text-base font-medium">
-          <ul className="flex gap-6">
+          <ul className="flex">
             {navLinks.map((link) => (
               <li key={link.href}>
-                <Link to={link.href}>{link.displayedName}</Link>
+                <Button className="text-base" variant={"link"}>
+                  <Link to={link.href}>{link.displayedName}</Link>
+                </Button>
               </li>
             ))}
           </ul>
@@ -31,7 +40,7 @@ export default function Header() {
 
         {/* Mobile Burger Icon */}
         <button
-          className="md:hidden z-20"
+          className="hover:scale-[1.02] transition-all focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring [&_svg]:pointer-events-none md:hidden z-20"
           onClick={() => setIsOpen((prev) => !prev)}
         >
           {isOpen ? <FiX size={28} /> : <FiMenu size={28} />}
@@ -40,16 +49,18 @@ export default function Header() {
 
       {/* Mobile Menu */}
       <div
-        className={`md:hidden overflow-hidden transition-all duration-300 ease-in-out ${
-          isOpen ? "max-h-60 pb-4" : "max-h-0"
+        className={`border-t md:hidden overflow-hidden transition-all duration-300 ease-in-out ${
+          isOpen ? "max-h-60 py-4" : "max-h-0"
         }`}
       >
         <ul className="flex flex-col items-center gap-4 text-base font-medium">
           {navLinks.map((link) => (
             <li key={link.href}>
-              <Link to={link.href} onClick={() => setIsOpen(false)}>
-                {link.displayedName}
-              </Link>
+              <Button className="text-base" variant={"link"}>
+                <Link to={link.href} onClick={() => setIsOpen(false)}>
+                  {link.displayedName}
+                </Link>
+              </Button>
             </li>
           ))}
         </ul>
