ae659e92177f513c44dfb9a7f8102fe856bebb0a
Alex Valle <alexvalle75@gmail.com>
2025-04-13 15:29:30 +0200

    Changed RootLayout to use Header and Footer

--- src/app-components/RootLayout.tsx
+++ src/app-components/RootLayout.tsx
@@ -1,10 +1,13 @@
 import { Outlet } from 'react-router-dom';
+import Header from '../_components/Header';
+import Footer from '../_components/Footer';
 
 export default function RootLayout() {
   return (
-    <div className="flex flex-col">
-      <h1>RootLayout</h1>
+    <div className="flex flex-col min-h-dvh">
+      <Header />
       <Outlet />
+      <Footer />
     </div>
   );
 }
