632cbafd3abf72ecf515af9cc12b6f3dbf78f082
Alex Valle <alexvalle75@gmail.com>
2025-04-13 15:34:58 +0200

    Added paragraph to homepage

--- src/pages/homepage/HomePage.tsx
+++ src/pages/homepage/HomePage.tsx
@@ -4,6 +4,10 @@ export default function HomePage() {
   return (
     <div>
       <Hero />
+
+      <p className="px-8 md:px-0">
+        Exploring Literary and Archaeological Insights on ancient Oil Studies
+      </p>
     </div>
   );
 }
